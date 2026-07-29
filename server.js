66e554e1a21724cf17dabbbc3f798609a6a10769
kailiat <nhoxbao20054@gmail.com>
2026-07-29 22:40:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -329,7 +329,41 @@ res.json({
 });
 
 
+// Check current login user
+app.get("/me", async (req, res) => {
 
+  try {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not logged in"
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    res.json({
+      success: true,
+      user: decoded
+    });
+
+
+  } catch (err) {
+
+    res.json({
+      success: false,
+      message: "Invalid token"
+    });
+
+  }
+
+});
 
 
 const PORT = process.env.PORT || 3000;
