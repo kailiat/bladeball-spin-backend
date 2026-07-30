fe2b55987c471963566c04a0b8a5b8194f0963d4
kailiat <nhoxbao20054@gmail.com>
2026-07-31 00:57:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:true,
+  credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
