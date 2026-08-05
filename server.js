3d7b2f51a2d2d56df80b8c7dbad781e633d5b3c6
kailiat <nhoxbao20054@gmail.com>
2026-08-05 21:13:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -729,7 +729,7 @@ app.post("/spin", async (req, res) => {
     return res.json({
       success: true,
       random,
-      reward,
+      reward: reward.label || `${reward.amount} Tokens`,
       balance: newBalance,
       spin_chances: newSpin
     });
